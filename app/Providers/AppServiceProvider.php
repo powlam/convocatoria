b5b290eac10c0a89cf906d80229c3ad158da3718
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-22 10:57:01 +0200

    Force root URL for all routes
    it allows serving the project from any IP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force root URL for all routes (even Livewire)
+        URL::forceRootUrl(Config::get('app.url'));
+        if (Str::contains(Config::get('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get($this->baseUri().'/livewire/livewire.js', $handle);
+        });
+    }
+
+    /**
+     * Extract the base URI from de configured app.url
+     * Ex: APP_URL=http://convocatoria.test => ''
+     * Ex: APP_URL=http://192.168.1.43/convocatoria.test/ => '/convocatoria.test'
+     * Ex: APP_URL=https://powlam.com/projects/convocatoria.test => '/projects/convocatoria.test'
+     */
+    private function baseUri(): string
+    {
+        $url = Str::chopStart(Config::get('app.url'), ['https://', 'http://']); // without schema
+        $url = Str::unwrap($url, '/'); // without starting and trailing bar
+
+        return Str::contains($url, '/') ? '/'.Str::after($url, '/') : '';
     }
 }
