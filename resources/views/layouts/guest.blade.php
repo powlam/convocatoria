7e7d68d6b2c38ad7775fb5b1167d3a4ff0244037
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-29 12:15:02 +0100

    app.name + app.version

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="version" content="{{ config('app.version') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
